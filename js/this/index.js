c3e5c52efc36b524b66ceeafed13f5053c8f4e5b
Alex Klyde <alexeykalayda@gmail.com>
2020-12-29 12:19:48 +0200

    this

--- js/this/index.js
+++ js/this/index.js
@@ -1,32 +1,58 @@
 const timer = {
   secondsPassed: 0,
   minsPassed: 0,
+  timerId: null,
 
+  /* startTimer() {
+    console.log('startTimer was called');
+
+    function handler() {
+      this.secondsPassed += 1;
+      console.log(this.secondsPassed);
+    }
+
+    // input: func, num(ms)
+    // output: num(id)
+    const handlerWithContext = handler.bind(this);
+
+    setInterval(handlerWithContext, 1000);
+    // window.handler();
+  }, */
+  // Option 2
   startTimer() {
-    window.intervId = setInterval(() => {
+    console.log('startTimer was called');
+
+    const timerId = setInterval(() => {
       if (this.secondsPassed === 60) {
         this.secondsPassed = 0;
         this.minsPassed += 1;
-        console.log(this.minsPassed)
       }
 
       this.secondsPassed += 1;
-    }, 1000)
-  },
 
-  counter() {
-    this.secondsPassed += 1;
+      // console.log('timerID' + timerId);
+      // console.log(this.secondsPassed);
+    }, 1000);
+
+    this.timerId = timerId;
   },
 
   getTime() {
-    return `${this.minsPassed}:${this.secondsPassed}`;
+    return this.secondsPassed < 10
+    ? `${this.minsPassed}:0${this.secondsPassed}`
+    : `${this.minsPassed}:${this.secondsPassed}`;
   },
 
   stopTimer() {
-    clearInterval(window.intervId);
+    clearInterval(this.timerId);
   },
 
   resetTimer() {
-    
+    this.secondsPassed = 0;
+    this.minsPassed = 0;
   },
-}
+};
+
+timer.startTimer();
+console.log(timer.secondsPassed);
+
